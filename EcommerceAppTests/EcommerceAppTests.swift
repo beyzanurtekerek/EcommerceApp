c3da37030301afde4b0c682774d929c8c4372f47
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-05-23 17:10:08 +0300

    Initial Commit

---
+++ EcommerceAppTests/EcommerceAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppTests.swift
+//  EcommerceAppTests
+//
+//  Created by Beyza Nur Tekerek on 23.05.2025.
+//
+
+import Testing
+@testable import EcommerceApp
+
+struct EcommerceAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
