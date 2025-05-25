10e524cb9dba61d92988522a5b5c39434f5a46d9
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-05-25 18:34:00 +0300

    home vc ui

--- EcommerceApp/Flow/Home/HomeViewController.swift
+++ EcommerceApp/Flow/Home/HomeViewController.swift
@@ -6,24 +6,63 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class HomeViewController: UIViewController {
-
+final class HomeViewController: BaseViewController {
+    
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search..."
+        searchBar.searchBarStyle = .minimal
+        searchBar.layer.cornerRadius = 6
+        searchBar.layer.masksToBounds = true
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        return searchBar
+    }()
+    
+    private let filterLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Filters:"
+        label.font = .systemFont(ofSize: 17, weight: .medium)
+        return label
+    }()
+    
+    private let filterButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Filter", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 6
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.systemGray6.cgColor
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
+        applyConstraints()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(filterButton)
+        view.addSubview(searchBar)
     }
-    */
-
+    
+    private func applyConstraints() {
+        let searchBarConstraints = [
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            searchBar.heightAnchor.constraint(equalToConstant: 40)
+        ]
+        
+        
+        NSLayoutConstraint.activate(searchBarConstraints)
+    }
+    
 }
