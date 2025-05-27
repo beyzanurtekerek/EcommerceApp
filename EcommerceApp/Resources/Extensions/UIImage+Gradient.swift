5e5b86dc1f250da58ab28ecc58ba8e9a02e51828
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-05-27 19:32:55 +0300

    add gradient extension

--- EcommerceApp/Resources/Extensions/UIImage+Gradient.swift
+++ EcommerceApp/Resources/Extensions/UIImage+Gradient.swift
@@ -5,4 +5,21 @@
 //  Created by Beyza Nur Tekerek on 27.05.2025.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    static func gradientImage(bounds: CGRect, colors: [UIColor]) -> UIImage? {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = colors.map { $0.cgColor }
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
+
+        UIGraphicsBeginImageContext(gradientLayer.bounds.size)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        gradientLayer.render(in: context)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+}
